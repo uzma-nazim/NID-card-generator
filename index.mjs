b6d66b77ee91ffb02cca193b8c42bbad456cca45
uzma <92790017+uzma-nazim@users.noreply.github.com>
2022-12-08 16:09:49 +0500

    NID-card-generator

--- index.mjs
+++ index.mjs
@@ -4,14 +4,14 @@ const require = createRequire(import.meta.url);
 const express = require("express")
 import route from "./routes/routes.mjs"
 const app = express();
-const cors = require("cors")
+
 
 
 const Port = 5000 || process.env.Port;
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors())
+
 
 
 
